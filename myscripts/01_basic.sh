b79a9b221d9c743312e65c237bcf2017c5dc0200
Santosh Baggon <baggonsantosh@gmail.com>
2024-11-30 11:28:31 +0530

    check commit

--- myscripts/01_basic.sh
+++ myscripts/01_basic.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
 echo "Hey santosh Baggon"
+<<comment
+
+This is called shebang (or hashbang)
+
+#!:
+
+The #! indicates that the script should be interpreted by a specific program.
+/bin/bash:
+
+This is the full path to the Bash shell on most Unix-like systems.
+It tells the system to use the Bash shell to interpret the commands in the script.
+
+comment
+
+
+
 
